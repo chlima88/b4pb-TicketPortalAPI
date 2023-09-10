963bb2c59f244c58fd9f5990bb9efab0e23ed163
Charles Costa <chlima88@gmail.com>
2023-09-10 12:54:34 -0300

    refactory da classe TicketController

--- controllers/ticket_controller.py
+++ controllers/ticket_controller.py
@@ -1,5 +1,5 @@
 from typing import Iterable
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from models.ticket import Ticket, TicketDTO
 from services.service import Service
@@ -8,26 +8,38 @@ from dataloaders.dataloader import DataLoader
 
 
 ticketController = APIRouter(prefix="/tickets")
-ticketRepository = Repository()
+ticketRepository = Repository(idSeed=50100)
 ticketService = Service[Ticket](ticketRepository)
 DataLoader.load(Ticket, ticketRepository, "./dataloaders/data/Tickets.csv")
 
 
 @ticketController.post("/", status_code=201)
-async def post(ticketDTO: TicketDTO) -> TicketDTO:
-    return ticketService.create(ticketDTO)
+async def post(ticketDTO: TicketDTO) -> Ticket:
+    try:
+        return ticketService.create(ticketDTO)
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=f"{error}")
 
 
-@ticketController.get("/{id}", status_code=200, response_model=TicketDTO)
-async def get(id: int) -> TicketDTO:
-    return ticketService.getById(id)
+@ticketController.get("/{id}", status_code=200)
+async def get(id: int) -> Ticket:
+    try:
+        return ticketService.getById(id)
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=f"{error}")
 
 
 @ticketController.get("/", status_code=200)
-async def getAll() -> Iterable[TicketDTO]:
-    return ticketService.getAll()
+async def getAll() -> Iterable[Ticket]:
+    try:
+        return ticketService.getAll()
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=f"{error}")
 
 
 @ticketController.delete("/{id}", status_code=204)
 async def delete(id: int) -> None:
-    ticketService.remove(id)
+    try:
+        ticketService.remove(id)
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=f"{error}")
